a0df8bdaf669042102ffe234c0d7833a2d965619
Martin Bartusek <bartusek@cryptelo.com>
2020-05-05 23:04:41 +0200

    make fronted code tolerable

--- api/public/graphs.js
+++ api/public/graphs.js
@@ -1,338 +1,208 @@
 am4core.useTheme(am4themes_animated);
 
-var green = '#00cc33';
-var red = '#b32400';
-var titleSize = 25;
-var titleMargin = 30;
+class Chart {
+  constructor(id, data) {
+    this.chart = am4core.create(id, am4charts.XYChart);
+    this.chart.data = data;
+    this.chart.cursor = new am4charts.XYCursor();
 
-///////////////
-//  xyCHART  //
-///////////////
+    this.titleSize = 25;
+    this.titleMargin = 30;
+    this.green = '#00cc33';
+    this.red = '#b32400';
+  }
 
-var chart = am4core.create("chartdiv", am4charts.XYChart);
-chart.hiddenState.properties.opacity = 0;
-let title = chart.titles.create();
-title.text = "Invested vs. actual";
-title.fontSize = titleSize;
-title.marginBottom = titleMargin;
+  setTitle(name, titleSize, titleMargin) {
+    let title = this.chart.titles.create();
+    title.text = name;
+    title.fontSize = titleSize || this.titleSize;
+    title.marginBottom = titleMargin || this.titleMargin;
+  }
 
-chart.data = xyChart;
+  setAxeX(am4axe) {
+    return this.chart.xAxes.push(new am4axe());
+  }
 
-var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+  setCategoryAxeX(category) {
+    let axe = this.chart.xAxes.push(new am4charts.CategoryAxis());
+    axe.dataFields.category = category;
+    axe.renderer.grid.template.location = 0;
+    axe.renderer.minGridDistance = 30;
+    axe.renderer.labels.template.horizontalCenter = "right";
+    axe.renderer.labels.template.verticalCenter = "middle";
+    axe.renderer.labels.template.rotation = 270;
+    axe.tooltip.disabled = true;
+    axe.renderer.minHeight = 110;
+    return axe;
+  }
 
-var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-valueAxis.tooltip.disabled = true;
+  setAxeY(am4axe) {
+    return this.chart.yAxes.push(new am4axe());
+  }
 
-var series = chart.series.push(new am4charts.LineSeries());
-series.dataFields.dateX = "date";
-series.dataFields.openValueY = "current";
-series.dataFields.valueY = "invested";
-series.tooltipText = "current: {openValueY.value} invested: {valueY.value}";
-series.sequencedInterpolation = true;
-series.fillOpacity = 0.3;
-series.defaultState.transitionDuration = 1000;
-series.tensionX = 0.8;
+  setSerie(am4serie, nameX, valueX, valueY, width, tooltip, opacity) {
+    let serie = this.chart.series.push(new am4serie());
+    serie.dataFields.valueY = valueY;
+    serie.dataFields[nameX] = valueX;
+    serie.strokeWidth = width;
+    serie.tooltipText = tooltip;
+    serie.fillOpacity = opacity;
+    return serie;
+  }
 
-var series2 = chart.series.push(new am4charts.LineSeries());
-series2.dataFields.dateX = "date";
-series2.dataFields.valueY = "current";
-series2.sequencedInterpolation = true;
-series2.defaultState.transitionDuration = 1500;
-series2.stroke = chart.colors.getIndex(6);
-series2.tensionX = 0.8;
+  setRange(axis, serie, positive) {
+    let range = axis.createSeriesRange(serie)
+    range.value = 0;
+    range.endValue = positive ? 1000000 : -1000000;
+    range.contents.stroke = am4core.color(positive ? this.green : this.red);
+    range.contents.fill = range.contents.stroke;
+    range.contents.strokeOpacity = 0.7;
+    range.contents.fillOpacity = 0.1;
+  }
 
-chart.cursor = new am4charts.XYCursor();
-chart.cursor.xAxis = dateAxis;
-chart.scrollbarX = new am4core.Scrollbar();
-
-
-///////////////
-//  BALANCE  //
-///////////////
-var balance = am4core.create("balance", am4charts.XYChart);
-let title2 = balance.titles.create();
-title2.text = "Balance";
-title2.fontSize = titleSize;
-title2.marginBottom = titleMargin;
-balance.data = balanceChart;
-
-// Create axes
-var dateAxis = balance.xAxes.push(new am4charts.DateAxis());
-dateAxis.startLocation = 0.5;
-dateAxis.endLocation = 0.5;
-
-// Create value axis
-var valueAxis = balance.yAxes.push(new am4charts.ValueAxis());
-
-// Create series
-var series = balance.series.push(new am4charts.LineSeries());
-series.dataFields.valueY = "balance";
-series.dataFields.dateX = "date";
-series.strokeWidth = 3;
-series.tooltipText = "{valueY.value}";
-series.fillOpacity = 0.1;
-
-var range = valueAxis.createSeriesRange(series);
-range.value = 0;
-range.endValue = -1000000;
-range.contents.stroke = am4core.color(red);
-range.contents.fill = range.contents.stroke;
-range.contents.strokeOpacity = 0.7;
-range.contents.fillOpacity = 0.1;
-
-var range2 = valueAxis.createSeriesRange(series);
-range2.value = 1000000;
-range2.endValue = 0;
-range2.contents.stroke = am4core.color(green);
-range2.contents.fill = range2.contents.stroke;
-range2.contents.strokeOpacity = 0.7;
-range2.contents.fillOpacity = 0.1;
-
-// Add cursor
-balance.cursor = new am4charts.XYCursor();
-balance.cursor.xAxis = dateAxis;
-balance.scrollbarX = new am4core.Scrollbar();
-
-
-
+  setScrollBarX() {
+    this.chart.scrollbarX = new am4core.Scrollbar();
+  }
+}
 
 ///////////////
 //  TODAY    //
 ///////////////
-// Create chart instance
-var today = am4core.create("today", am4charts.XYChart);
-chart.scrollbarX = new am4core.Scrollbar();
-let title3 = today.titles.create();
-title3.text = "Today change " + todayTitle;
-title3.fontSize = titleSize;
-title3.marginBottom = titleMargin;
+let today = new Chart('today', todayChart)
+today.setTitle('Today change ' + todayTitle)
+let todayAxeX = today.setCategoryAxeX('name')
+let todayAxeY = today.setAxeY(am4charts.ValueAxis)
+todayAxeY.renderer.minWidth = 50;
 
-// Add data
-today.data = todayChart;
+let todaySerie = today.setSerie(am4charts.ColumnSeries,
+  'categoryX', 'name', 'balance', 3, '[{categoryX}: bold]{valueY}[/]', 0.8)
+todaySerie.sequencedInterpolation = true;
+todaySerie.columns.template.strokeWidth = 0;
+todaySerie.tooltip.pointerOrientation = "vertical";
+todaySerie.columns.template.column.fillOpacity = 0.8;
 
-// Create axes
-var categoryAxis = today.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "name";
-categoryAxis.renderer.grid.template.location = 0;
-categoryAxis.renderer.minGridDistance = 30;
-categoryAxis.renderer.labels.template.horizontalCenter = "right";
-categoryAxis.renderer.labels.template.verticalCenter = "middle";
-categoryAxis.renderer.labels.template.rotation = 270;
-categoryAxis.tooltip.disabled = true;
-categoryAxis.renderer.minHeight = 110;
+let todayHoverState = todaySerie.columns.template.column.states.create("hover");
+todayHoverState.properties.fillOpacity = 1;
 
-var valueAxis = today.yAxes.push(new am4charts.ValueAxis());
-valueAxis.renderer.minWidth = 50;
-
-// Create series
-var series = today.series.push(new am4charts.ColumnSeries());
-series.sequencedInterpolation = true;
-series.dataFields.valueY = "balance";
-series.dataFields.categoryX = "name";
-series.tooltipText = "[{categoryX}: bold]{valueY}[/]";
-series.columns.template.strokeWidth = 0;
-
-series.tooltip.pointerOrientation = "vertical";
-series.columns.template.column.fillOpacity = 0.8;
-
-let hoverState = series.columns.template.column.states.create("hover");
-hoverState.properties.fillOpacity = 1;
-
-var range3 = valueAxis.createSeriesRange(series);
-range3.value = 1000000;
-range3.endValue = 0;
-range3.contents.stroke = am4core.color(green);
-range3.contents.fill = range3.contents.stroke;
-range3.contents.strokeOpacity = 0.7;
-range3.contents.fillOpacity = 0.1;
-
-var range4 = valueAxis.createSeriesRange(series);
-range4.value = -1000000;
-range4.endValue = 0;
-range4.contents.stroke = am4core.color(red);
-range4.contents.fill = range4.contents.stroke;
-range4.contents.strokeOpacity = 0.7;
-range4.contents.fillOpacity = 0.1;
-
-// Cursor
-today.cursor = new am4charts.XYCursor();
-
+today.setRange(todayAxeY, todaySerie, false)
+today.setRange(todayAxeY, todaySerie, true)
 
 
 ///////////////
 //  STOCKS   //
 ///////////////
-// Create chart instance
-var stocks = am4core.create("stocks", am4charts.XYChart);
-chart.scrollbarX = new am4core.Scrollbar();
-let title4 = stocks.titles.create();
-title4.text = "Stocks performance";
-title4.fontSize = titleSize;
-title4.marginBottom = titleMargin;
+let stocks = new Chart('stocks', stockChart);
+stocks.setTitle('Stocks performance');
+let stocksAxeX = stocks.setCategoryAxeX('name')
+let stocksAxeY = stocks.setAxeY(am4charts.ValueAxis)
+stocksAxeY.renderer.minWidth = 50;
 
-// Add data
-stocks.data = stockChart;
+let stocksSerie = stocks.setSerie(am4charts.ColumnSeries,
+  'categoryX', 'name', 'balance', 3, '[{categoryX}: bold]{valueY}[/]', 0.8)
+stocksSerie.sequencedInterpolation = true;
+stocksSerie.columns.template.strokeWidth = 0;
+stocksSerie.tooltip.pointerOrientation = "vertical";
+stocksSerie.columns.template.column.fillOpacity = 0.8;
 
-// Create axes
-var categoryAxis2 = stocks.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis2.dataFields.category = "name";
-categoryAxis2.renderer.grid.template.location = 0;
-categoryAxis2.renderer.minGridDistance = 30;
-categoryAxis2.renderer.labels.template.horizontalCenter = "right";
-categoryAxis2.renderer.labels.template.verticalCenter = "middle";
-categoryAxis2.renderer.labels.template.rotation = 270;
-categoryAxis2.tooltip.disabled = true;
-categoryAxis2.renderer.minHeight = 110;
+let stocksHoverState = stocksSerie.columns.template.column.states.create("hover");
+stocksHoverState.properties.fillOpacity = 1;
 
-var valueAxis2 = stocks.yAxes.push(new am4charts.ValueAxis());
-valueAxis2.renderer.minWidth = 50;
+stocks.setRange(stocksAxeY, stocksSerie, false)
+stocks.setRange(stocksAxeY, stocksSerie, true)
 
-// Create series
-var stockSeries = stocks.series.push(new am4charts.ColumnSeries());
-stockSeries.sequencedInterpolation = true;
-stockSeries.dataFields.valueY = "balance";
-stockSeries.dataFields.categoryX = "name";
-stockSeries.tooltipText = "[{categoryX}: bold]{valueY}[/]";
-stockSeries.columns.template.strokeWidth = 0;
+////////////////
+//  INVESTED  //
+////////////////
+let invested = new Chart('chartdiv', xyChart);
+invested.setTitle('Invested vs. current value');
+invested.setScrollBarX();
+invested.chart.hiddenState.properties.opacity = 0;
 
-stockSeries.tooltip.pointerOrientation = "vertical";
+let investedAxeX = invested.setAxeX(am4charts.DateAxis)
+let investedAxeY = invested.setAxeY(am4charts.ValueAxis);
+investedAxeY.tooltip.disabled = true;
 
-stockSeries.columns.template.column.fillOpacity = 0.8;
+let investedSerie = invested.setSerie(am4charts.LineSeries, 'dateX', 'date',
+  'invested', 1, 'current: {openValueY.value} invested: {valueY.value}', 0.3)
+investedSerie.dataFields.openValueY = "current";
+investedSerie.sequencedInterpolation = true;
+investedSerie.defaultState.transitionDuration = 1500;
+investedSerie.tensionX = 0.8;
 
-let hoverState2 = stockSeries.columns.template.column.states.create("hover");
-hoverState2.properties.fillOpacity = 1;
+let investedCurrentSerie = invested.setSerie(am4charts.LineSeries, 'dateX', 'date',
+  'current', 2, '', 0)
 
-var range5 = valueAxis2.createSeriesRange(stockSeries);
-range5.value = 1000000;
-range5.endValue = 0;
-range5.contents.stroke = am4core.color(green);
-range5.contents.fill = range5.contents.stroke;
-range5.contents.strokeOpacity = 0.7;
-range5.contents.fillOpacity = 0.1;
+investedCurrentSerie.sequencedInterpolation = true;
+investedCurrentSerie.defaultState.transitionDuration = 1500;
+investedCurrentSerie.stroke = invested.chart.colors.getIndex(6);
+investedCurrentSerie.tensionX = 0.8;
 
-var range6 = valueAxis2.createSeriesRange(stockSeries);
-range6.value = -1000000;
-range6.endValue = 0;
-range6.contents.stroke = am4core.color(red);
-range6.contents.fill = range6.contents.stroke;
-range6.contents.strokeOpacity = 0.7;
-range6.contents.fillOpacity = 0.1;
+invested.chart.cursor.xAxis = investedAxeX;
 
-// Cursor
-stocks.cursor = new am4charts.XYCursor();
+
+///////////////
+//  BALANCE  //
+///////////////
+let balance = new Chart('balance', balanceChart);
+balance.setTitle('Gains/loses');
+balance.setScrollBarX();
+let balanceAxeX = balance.setAxeX(am4charts.DateAxis)
+balanceAxeX.startLocation = 0.5;
+balanceAxeX.endLocation = 0.5;
+let balanceAxeY = balance.setAxeY(am4charts.ValueAxis);
+let balanceSerie = balance.setSerie(am4charts.LineSeries,
+  'dateX', 'date', 'balance', 3, '{valueY.value}', 0.1)
+balance.setRange(balanceAxeY, balanceSerie, false)
+balance.setRange(balanceAxeY, balanceSerie, true)
+balance.chart.cursor.xAxis = balanceAxeX;
 
 
 ///////////////////////
 // CURRENCY BALANCE  //
 ///////////////////////
-var balance = am4core.create("currency", am4charts.XYChart);
-let title7 = balance.titles.create();
-title7.text = "Currency balance";
-title7.fontSize = titleSize;
-title7.marginBottom = titleMargin;
-balance.data = currencyBalanceChart;
+let currencyBalance = new Chart('currency', currencyBalanceChart);
+currencyBalance.setTitle('Currency balance');
+currencyBalance.setScrollBarX();
+let currencyBalanceAxeX = currencyBalance.setAxeX(am4charts.DateAxis)
+currencyBalanceAxeX.startLocation = 0.5;
+currencyBalanceAxeX.endLocation = 0.5;
+let currencyBalanceAxeY = currencyBalance.setAxeY(am4charts.ValueAxis);
 
-// Create axes
-var dateAxis = balance.xAxes.push(new am4charts.DateAxis());
-dateAxis.startLocation = 0.5;
-dateAxis.endLocation = 0.5;
+let currencyBalanceSerie = currencyBalance.setSerie(am4charts.LineSeries,
+  'dateX', 'date', 'balance', 3, '{valueY.value} CZK (all)', 0.1)
+currencyBalance.setRange(currencyBalanceAxeY, currencyBalanceSerie, false)
+currencyBalance.setRange(currencyBalanceAxeY, currencyBalanceSerie, true)
 
-// Create value axis
-var valueAxis = balance.yAxes.push(new am4charts.ValueAxis());
+currencyBalance.setSerie(am4charts.LineSeries,
+  'dateX', 'date', 'usd_balance', 1, '{valueY.value} CZK ($)', 0)
+currencyBalance.setSerie(am4charts.LineSeries,
+  'dateX', 'date', 'eur_balance', 1, '{valueY.value} CZK (€)', 0)
 
-// Create series
-var series = balance.series.push(new am4charts.LineSeries());
-series.dataFields.valueY = "balance";
-series.dataFields.dateX = "date";
-series.strokeWidth = 3;
-series.tooltipText = "{valueY.value} CZK (all)";
-series.fillOpacity = 0.1;
-
-var range8 = valueAxis.createSeriesRange(series);
-range8.value = 0;
-range8.endValue = -1000000;
-range8.contents.stroke = am4core.color(red);
-range8.contents.fill = range8.contents.stroke;
-range8.contents.strokeOpacity = 0.7;
-range8.contents.fillOpacity = 0.1;
-
-var range7 = valueAxis.createSeriesRange(series);
-range7.value = 1000000;
-range7.endValue = 0;
-range7.contents.stroke = am4core.color(green);
-range7.contents.fill = range7.contents.stroke;
-range7.contents.strokeOpacity = 0.7;
-range7.contents.fillOpacity = 0.1;
-
-var serie_usd = balance.series.push(new am4charts.LineSeries());
-serie_usd.dataFields.valueY = "usd_balance";
-serie_usd.dataFields.dateX = "date";
-serie_usd.strokeWidth = 1;
-serie_usd.tooltipText = "{valueY.value} CZK ($)";
-serie_usd.fillOpacity = 0;
-
-var serie_eur = balance.series.push(new am4charts.LineSeries());
-serie_eur.dataFields.valueY = "eur_balance";
-serie_eur.dataFields.dateX = "date";
-serie_eur.strokeWidth = 1;
-serie_eur.tooltipText = "{valueY.value} CZK (€)";
-serie_eur.fillOpacity = 0;
-
-// Add cursor
-balance.cursor = new am4charts.XYCursor();
-balance.cursor.xAxis = dateAxis;
-balance.scrollbarX = new am4core.Scrollbar();
+currencyBalance.chart.cursor.xAxis = currencyBalanceAxeX;
 
 
+/////////////////////////
+//  STOCKS BY CURRENCY //
+/////////////////////////
+let stocksByCurrency = new Chart('currency_totals', xyChart);
+stocksByCurrency.setTitle('Stocks by currency');
+stocksByCurrency.setScrollBarX();
 
-///////////////////////
-//  CURRENCY TOTALS  //
-///////////////////////
-var totals = am4core.create("currency_totals", am4charts.XYChart);
-let title8 = totals.titles.create();
-title8.text = "Currency split";
-title8.fontSize = titleSize;
-title8.marginBottom = titleMargin;
-totals.data = xyChart;
+let stocksByCurrencyAxeX = stocksByCurrency.setAxeX(am4charts.DateAxis)
+stocksByCurrencyAxeX.startLocation = 0.5;
+stocksByCurrencyAxeX.endLocation = 0.5;
+let stocksByCurrencyAxeY = stocksByCurrency.setAxeY(am4charts.ValueAxis);
 
-// Create axes
-var dateAxis = totals.xAxes.push(new am4charts.DateAxis());
-dateAxis.startLocation = 0.5;
-dateAxis.endLocation = 0.5;
+stocksByCurrency.setSerie(am4charts.LineSeries,
+  'dateX', 'date', 'current', 3, '{valueY.value} CZK (all)', 0)
 
-// Create value axis
-var valueAxis = totals.yAxes.push(new am4charts.ValueAxis());
+stocksByCurrency.setSerie(am4charts.LineSeries,
+  'dateX', 'date', 'usd_value', 2, '{valueY.value} CZK ($)', 0)
 
-// Create series
-var series7 = totals.series.push(new am4charts.LineSeries());
-series7.dataFields.valueY = "current";
-series7.dataFields.dateX = "date";
-series7.strokeWidth = 3;
-series7.tooltipText = "{valueY.value} CZK (all)";
-series7.fillOpacity = 0;
+stocksByCurrency.setSerie(am4charts.LineSeries,
+  'dateX', 'date', 'eur_value', 2, '{valueY.value} CZK (€)', 0)
 
-var series8 = totals.series.push(new am4charts.LineSeries());
-series8.dataFields.valueY = "usd_value";
-series8.dataFields.dateX = "date";
-series8.strokeWidth = 3;
-series8.tooltipText = "{valueY.value}  CZK ($)";
-series8.fillOpacity = 0;
+stocksByCurrency.setSerie(am4charts.LineSeries,
+  'dateX', 'date', 'other_value', 2, '{valueY.value} CZK (other)', 0)
 
-var series9 = totals.series.push(new am4charts.LineSeries());
-series9.dataFields.valueY = "eur_value";
-series9.dataFields.dateX = "date";
-series9.strokeWidth = 3;
-series9.tooltipText = "{valueY.value}  CZK (€)";
-series9.fillOpacity = 0;
-
-var series10 = totals.series.push(new am4charts.LineSeries());
-series10.dataFields.valueY = "other_value";
-series10.dataFields.dateX = "date";
-series10.strokeWidth = 3;
-series10.tooltipText = "{valueY.value}  CZK (other)";
-series10.fillOpacity = 0;
-
-// Add cursor
-totals.cursor = new am4charts.XYCursor();
-totals.cursor.xAxis = dateAxis;
-totals.scrollbarX = new am4core.Scrollbar();
+stocksByCurrency.chart.cursor.xAxis = stocksByCurrencyAxeX;
