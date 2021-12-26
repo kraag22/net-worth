b7ab4eb0c7a87b36b2d67852c7040efe81dce828
Martin Bartušek <kraag22@martins-macbook-pro-3.home>
2021-12-26 19:34:04 +0100

    add another reality data point

--- api/src/constants.js
+++ api/src/constants.js
@@ -4,3 +4,4 @@ exports.NAME_MAX_LENGTH = 10
 
 exports.jihlava_2_rooms_url = "https://www.sreality.cz/hledani/prodej/byty/jihlava?velikost=2%2B1,2%2Bkk&stav=pred-rekonstrukci,po-rekonstrukci,spatny-stav,dobry-stav&bez-aukce=1"
 exports.praha_3_4_rooms_url = "https://www.sreality.cz/hledani/prodej/byty/praha-7?velikost=4%2B1,4%2Bkk,3%2B1,3%2Bkk&stavba=cihlova&vlastnictvi=osobni&stav=pred-rekonstrukci,po-rekonstrukci,spatny-stav,dobry-stav,velmi-dobry-stav&plocha-od=80&plocha-do=10000000000&bez-aukce=1"
+exports.praha_1_rooms_url = "https://www.sreality.cz/hledani/prodej/byty/praha-7?velikost=1%2B1,1%2Bkk&stavba=cihlova&vlastnictvi=osobni&stav=po-rekonstrukci,dobry-stav,velmi-dobry-stav&plocha-od=20&plocha-do=40&bez-aukce=1"
