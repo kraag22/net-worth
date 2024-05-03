91e7f35911a26ae3aa87178e8a11b71befc5c91f
Martin Bartušek <martinbartusek@gmail.com>
2024-05-03 14:56:02 +0200

    login to seznam before accessing sreality server
    so we doesn't have to deal with consent page hidden in shadow-root
    closed

--- api/src/reality/scrapper.js
+++ api/src/reality/scrapper.js
@@ -1,9 +1,19 @@
 const puppeteer = require('puppeteer')
 
 exports.makeRequest = async (url) => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
-  await page.goto(url)
+
+  await page.goto('https://login.szn.cz', { waitUntil: 'networkidle0' })
+
+  await page.type('#login-username', process.env.seznam_email)
+  await page.keyboard.press('Enter')
+  await page.type('#login-password', process.env.seznam_password)
+  await page.keyboard.press('Enter')
+
+  await page.waitForSelector('span.bullet')
+
+  await page.goto(url, { waitUntil: 'networkidle0' })
   let html = await page.content()
 
   await browser.close()
